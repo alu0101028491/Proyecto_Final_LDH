7e9ba2b8053919e181f9de0c593ff18043083535
Dario Afonso <alu0100406580@ull.edu.es>
2022-12-01 11:37:43 +0000

    Actualizacion .gitignore

--- src/main/java/recommendation_system/recommenders/RecommendationRunner.java
+++ src/main/java/recommendation_system/recommenders/RecommendationRunner.java
@@ -1,5 +1,6 @@
 package recommendation_system.recommenders;
 
+import recommendation_system.FourthRatings;
 import recommendation_system.FourthRatingsOptimizedByWeiXu;
 import recommendation_system.MovieDatabase;
 import recommendation_system.RaterDatabase;
@@ -55,12 +56,12 @@ public class RecommendationRunner implements Recommender {
         }
         return movieToBeRate;
     }
-    
+
     @Override
     public void printRecommendationsFor(String webRaterID) {
         MovieDatabase.initialize("ratedmoviesfull.csv");
         RaterDatabase.initialize("ratings.csv");
-        FourthRatingsOptimizedByWeiXu fr = new FourthRatingsOptimizedByWeiXu();
+        FourthRatings fr = new FourthRatings();
         ArrayList<Rating> ratingList = fr.getSimilarRatings(webRaterID, 20, 5);
         //System.out.println("Found ratings for movies : " + ratingList.size());
         if (ratingList.size() == 0) {
@@ -73,14 +74,14 @@ public class RecommendationRunner implements Recommender {
                 if (!movieToBeRate.contains(ratingList.get(i).getItem())) {
                     outID.add(ratingList.get(i));
                     //System.out.println("i = " + i + " id = " + ratingList.get(i).getItem());
-                    
+
                 } else {
                     count++;
                 }
             }
             System.out.println("outid size = " + outID.size());
-            
-            
+
+
             System.out.println("<style>");
             System.out.println("h2,h3{");
             System.out.println("  text-align: center;");
@@ -89,14 +90,14 @@ public class RecommendationRunner implements Recommender {
             System.out.println("  font-family: Arial, Helvetica, sans- serif;");
             System.out.println("  background-color: black;");
             System.out.println("   color:  #ff6600 }");
-            
+
             System.out.println(" table {");
             System.out.println("   border-collapse: collapse;");
             System.out.println("   margin: auto;}");
             System.out.println("table, th, td {");
             System.out.println("    border: 2px solid white;");
             System.out.println("    font-size: 15px;");
-            
+
             System.out.println("    padding: 2px 6px 2px 6px; }");
             System.out.println(" td img{");
             System.out.println("    display: block;");
@@ -105,11 +106,11 @@ public class RecommendationRunner implements Recommender {
             System.out.println("th {");
             System.out.println("    height: 40px;");
             System.out.println("    font-size: 18px;");
-            
+
             System.out.println("  background-color: black;");
             System.out.println(" color: white;");
             System.out.println("text-align: center; }");
-            
+
             System.out.println(" tr:nth-child(even) {");
             System.out.println("     background-color: #f2f2f2; }");
             System.out.println("  tr:nth-child(odd) {");
@@ -117,10 +118,10 @@ public class RecommendationRunner implements Recommender {
             System.out.println(" tr:hover {");
             System.out.println(" background-color: #666666; ");
             System.out.println("  color:white;}");
-            
+
             System.out.println("table td:first-child {");
             System.out.println(" text-align: center; }");
-            
+
             System.out.println(" tr {");
             System.out.println(" font-family: Arial, Helvetica, sans-serif; }");
             System.out.println(".rating{");
@@ -128,19 +129,19 @@ public class RecommendationRunner implements Recommender {
             System.out.println("    padding: 0px 10px;");
             System.out.println("   font-weight: bold; }");
             System.out.println("</style>");
-            
-            
+
+
             System.out.println("<h2>Wei Xu Brings Best Movies for You! Enjoy!^^</h2>");
             System.out.println("<table id = \"rater\">");
             System.out.println("<tr>");
             System.out.println("<th>Rank</th>");
-            
+
             System.out.println("<th>Poster</th>");
             System.out.println("<th>Title & Rating</th>");
             System.out.println("<th>Genre</th>");
             System.out.println("<th>Country</th>");
             System.out.println("</tr>");
-            
+
             //https://www.imdb.com/title/tt0780622/
             //make title chickable
             //<td><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Reference" >Hello World</a></td>
@@ -148,7 +149,7 @@ public class RecommendationRunner implements Recommender {
             int rank = 1;
             for (Rating i : outID) {
                 System.out.println("<tr><td>" + rank + "</td>" +
-                        
+
                         "<td><img src = \"" + MovieDatabase.getPoster(i.getItem()) + "\" width=\"50\" height=\"70\"></td> " +
                         "<td>" + MovieDatabase.getYear(i.getItem()) + "&ensp;&ensp; <a href=\"https://www.imdb.com/title/tt" +
                         i.getItem() + "\">" + MovieDatabase.getTitle(i.getItem()) + "</a><br><div class = \"rating\">&starf; &ensp;&ensp;&ensp;"
@@ -162,13 +163,13 @@ public class RecommendationRunner implements Recommender {
         System.out.println("</table>");
         System.out.println("<h3>*The rank of movies is based on other raters who have the most similar rating to yours. Enjoy!^^</h3>");
     }
-    
-    
-    //    public static void main(String[] args) {
-    //        RecommendationRunner a = new RecommendationRunner();
-    //        a.getItemsToRate();
-    //        a.printRecommendationsFor("65");
-    //
-    //    }
-    
+
+
+        public static void main(String[] args) {
+            RecommendationRunner a = new RecommendationRunner();
+            a.getItemsToRate();
+            a.printRecommendationsFor("30");
+
+        }
+
 }
