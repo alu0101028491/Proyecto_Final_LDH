9174345a018797354039d41af3faa627825adfea
Dario Afonso <alu0100406580@ull.edu.es>
2022-12-08 14:29:38 +0000

    SonarCloud - CodeSmells cambio logger

--- src/main/java/recommendation_system/movierunner/MovieRunnerAverage.java
+++ src/main/java/recommendation_system/movierunner/MovieRunnerAverage.java
@@ -6,6 +6,8 @@ import recommendation_system.ratings.SecondRatings;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import java.lang.System.Logger;
+
 /***************************************************************
  *  Name:    Wei Xu
  *
@@ -24,33 +26,39 @@ import java.util.Collections;
  *
  ****************************************************************/
 public class MovieRunnerAverage {
+
+	private Logger logger = System.getLogger(MovieRunnerAverage.class.getName());
+
+	private  static Logger loggerStatic = System.getLogger(MovieRunnerAverage.class.getName());
+
+
     public void printAverageRatings() {
         SecondRatings sr = new SecondRatings("data/ratedmovies_short.csv", "data/ratings_short.csv");//do i need put filename here?
-        System.out.println("Movie size = " + sr.getMovieSize());
-        System.out.println("Rater size = " + sr.getRaterSize());
+        logger.log(Logger.Level.INFO, "Movie size = " + sr.getMovieSize());
+        logger.log(Logger.Level.INFO, "Rater size = " + sr.getRaterSize());
         ArrayList<Rating> ratingList = sr.getAverageRatings(2);
         Collections.sort(ratingList);
         for (Rating i : ratingList) {
-            System.out.printf("%-10.2f%s%n", i.getValue(), sr.getTitle(i.getItem()));
+        	logger.log(Logger.Level.INFO, "%-10.2f%s%n", i.getValue(), sr.getTitle(i.getItem()));
         }
     }
-    
+
     public void getAverageRatingOneMovie() {
         SecondRatings sr = new SecondRatings("data/ratedmovies_short.csv", "data/ratings_short.csv");//do i need put filename here?
         ArrayList<Rating> ratingList = sr.getAverageRatings(2);
         String movieTitle = "The Godfather";
         for (Rating i : ratingList) {
             if (sr.getTitle(i.getItem()).equals(movieTitle)) {
-                System.out.printf("%-10.2f%s%n", i.getValue(), sr.getTitle(i.getItem()));
+            	logger.log(Logger.Level.INFO, "%-10.2f%s%n", i.getValue(), sr.getTitle(i.getItem()));
             }
         }
     }
-    
+
     public static void main(String[] args) {
         MovieRunnerAverage mra = new MovieRunnerAverage();
-        System.out.println("---------------print test----------------");
+        loggerStatic.log(Logger.Level.INFO, "---------------print test----------------");
         mra.printAverageRatings();
-        System.out.println("---------------get average rating one movie test----------------");
+        loggerStatic.log(Logger.Level.INFO, "---------------get average rating one movie test----------------");
         mra.getAverageRatingOneMovie();
     }
 }
