82a4e398052e499721a539ff6c4c9ce74ae3b570
Dario Afonso <alu0100406580@ull.edu.es>
2022-12-10 17:39:53 +0000

    Mejora - Generación index.html arreglo imagenes

--- src/main/java/recommendation_system/recommenders/RecommendationRunner.java
+++ src/main/java/recommendation_system/recommenders/RecommendationRunner.java
@@ -1,6 +1,7 @@
 package recommendation_system.recommenders;
 
 import recommendation_system.ratings.FourthRatings;
+import recommendation_system.ratings.FourthRatingsOptimizedByWeiXu;
 import recommendation_system.movies.MovieDatabase;
 import recommendation_system.raters.RaterDatabase;
 import recommendation_system.ratings.Rating;
@@ -9,6 +10,13 @@ import recommendation_system.filters.TrueFilter;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Random;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.io.File;
+import java.io.IOException;
 
 /***************************************************************
  *  Name:    Wei Xu
@@ -41,6 +49,10 @@ import java.util.Random;
 public class RecommendationRunner implements Recommender {
 
 	private Random rand = new SecureRandom();
+
+	private String filename = "index.html";
+	private Path pathToFile = Paths.get(filename);
+
     /**
      * randomly pick 10 movie to let the user to rate.
      * will optimize soon to rate the most popular movie.
@@ -61,6 +73,104 @@ public class RecommendationRunner implements Recommender {
         return movieToBeRate;
     }
 
+    public void writeHtmlHead() throws IOException {
+
+    	String textHead = "<!DOCTYPE html>\n"
+    			+ "<html lang=\"es\">\n"
+    			+ "<head>\n"
+    			+ "  <meta charset=\"UTF-8\">\n"
+    			+ "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+    			+ "  <title>LPH-Final</title>";
+
+
+    	Files.writeString(pathToFile, textHead, StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+
+    }
+
+    public void writeCss() throws IOException {
+
+
+    	String textCss = "  <style>\n"
+    			+ "    h2,h3{\n"
+    			+ "      text-align: center;\n"
+    			+ "      height: 50px;\n"
+    			+ "      line-height: 50px;\n"
+    			+ "      font-family: Arial, Helvetica, sans- serif;\n"
+    			+ "      background-color: black;\n"
+    			+ "       color:  #ff6600 }\n"
+    			+ "     table {\n"
+    			+ "       border-collapse: collapse;\n"
+    			+ "       margin: auto;}\n"
+    			+ "    table, th, td {\n"
+    			+ "        border: 2px solid white;\n"
+    			+ "        font-size: 15px;\n"
+    			+ "        padding: 2px 6px 2px 6px; }\n"
+    			+ "     td img{\n"
+    			+ "        display: block;\n"
+    			+ "        margin-left: auto;\n"
+    			+ "        margin-right: auto; }\n"
+    			+ "    th {\n"
+    			+ "        height: 40px;\n"
+    			+ "        font-size: 18px;\n"
+    			+ "      background-color: black;\n"
+    			+ "     color: white;\n"
+    			+ "    text-align: center; }\n"
+    			+ "     tr:nth-child(even) {\n"
+    			+ "         background-color: #f2f2f2; }\n"
+    			+ "      tr:nth-child(odd) {\n"
+    			+ "    background-color: #cccccc; }\n"
+    			+ "     tr:hover {\n"
+    			+ "     background-color: #666666; \n"
+    			+ "      color:white;}\n"
+    			+ "    table td:first-child {\n"
+    			+ "     text-align: center; }\n"
+    			+ "     tr {\n"
+    			+ "     font-family: Arial, Helvetica, sans-serif; }\n"
+    			+ "    .rating{\n"
+    			+ "        color:#ff6600;\n"
+    			+ "        padding: 0px 10px;\n"
+    			+ "       font-weight: bold; }\n"
+    			+ "    </style>\n"
+    			+ "</head>";
+
+    	Files.writeString(pathToFile, textCss, StandardOpenOption.APPEND);
+    }
+
+    public void writeBody(ArrayList<Rating> outID) throws IOException {
+
+    	String textBody = "<body>\n"
+    			+ "  <h2>Wei Xu Brings Best Movies for You! Enjoy!^^</h2>\n"
+    			+ "  <table id = \"rater\">\n"
+    			+ "  <tr>\n"
+    			+ "  <th>Rank</th>\n"
+    			+ "  <th>Poster</th>\n"
+    			+ "  <th>Title & Rating</th>\n"
+    			+ "  <th>Genre</th>\n"
+    			+ "  <th>Country</th>\n"
+    			+ "  </tr>\n"
+    			+ "  <tr>\n";
+
+
+    			int rank = 1;
+                for (Rating i : outID) {
+                	textBody += "      <tr><td>" + rank + "</td>" +
+                            "<td><img src = \"" + MovieDatabase.getPoster(i.getItem()).replace("http", "https") + "\" width=\"50\" height=\"70\"></td> " +
+                            "<td>" + MovieDatabase.getYear(i.getItem()) + "&ensp;&ensp; <a href=\"https://www.imdb.com/title/tt" +
+                            i.getItem() + "\">" + MovieDatabase.getTitle(i.getItem()) + "</a><br><div class = \"rating\">&starf; &ensp;&ensp;&ensp;"
+                            + String.format("%.1f", i.getValue()) + "/10</td>" +
+                            "<td>" + MovieDatabase.getGenres(i.getItem()) + "</td>" +
+                            "<td>" + MovieDatabase.getCountry(i.getItem()) + "</td>" +
+                            "</tr>\n";
+                    rank++;
+                }
+
+    		textBody += "  </table>\n"
+    				+ "  <h3>*The rank of movies is based on other raters who have the most similar rating to yours. Enjoy!^^</h3>";
+    		Files.writeString(pathToFile, textBody, StandardOpenOption.APPEND);
+
+    }
+
+
     @Override
     public void printRecommendationsFor(String webRaterID) {
         MovieDatabase.initialize("ratedmoviesfull.csv");
@@ -68,7 +178,6 @@ public class RecommendationRunner implements Recommender {
         //FourthRatingsOptimizedByWeiXu fr = new FourthRatingsOptimizedByWeiXu();
         FourthRatings fr = new FourthRatings();
         ArrayList<Rating> ratingList = fr.getSimilarRatings(webRaterID, 20, 5);
-        //System.out.println("Found ratings for movies : " + ratingList.size());
         if (ratingList.size() == 0) {
             System.out.println("<h2>Sorry, there are no movie recommend for you based on your rating!</h2>");
         } else {
@@ -78,8 +187,6 @@ public class RecommendationRunner implements Recommender {
             for (int i = 0; outID.size() + count != ratingList.size() && outID.size() < 10; i++) {
                 if (!movieToBeRate.contains(ratingList.get(i).getItem())) {
                     outID.add(ratingList.get(i));
-                    //System.out.println("i = " + i + " id = " + ratingList.get(i).getItem());
-
                 } else {
                     count++;
                 }
@@ -147,15 +254,11 @@ public class RecommendationRunner implements Recommender {
             System.out.println("<th>Country</th>");
             System.out.println("</tr>");
 
-            //https://www.imdb.com/title/tt0780622/
-            //make title chickable
-            //<td><a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Reference" >Hello World</a></td>
-            //"<a href="https://www.imdb.com/title/tt"+movie.id+"\">"+ movie.title+"</a></td>
             int rank = 1;
             for (Rating i : outID) {
                 System.out.println("<tr><td>" + rank + "</td>" +
 
-                        "<td><img src = \"" + MovieDatabase.getPoster(i.getItem()) + "\" width=\"50\" height=\"70\"></td> " +
+                        "<td><img src = \"" + MovieDatabase.getPoster(i.getItem()).replace("http", "https") + "\" width=\"50\" height=\"70\"></td> " +
                         "<td>" + MovieDatabase.getYear(i.getItem()) + "&ensp;&ensp; <a href=\"https://www.imdb.com/title/tt" +
                         i.getItem() + "\">" + MovieDatabase.getTitle(i.getItem()) + "</a><br><div class = \"rating\">&starf; &ensp;&ensp;&ensp;"
                         + String.format("%.1f", i.getValue()) + "/10</td>" +
@@ -164,15 +267,21 @@ public class RecommendationRunner implements Recommender {
                         "</tr> ");
                 rank++;
             }
+            try {
+            	writeHtmlHead();
+            	writeCss();
+            	writeBody(outID);
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}
         }
         System.out.println("</table>");
         System.out.println("<h3>*The rank of movies is based on other raters who have the most similar rating to yours. Enjoy!^^</h3>");
-    }
 
+    }
         public static void main(String[] args) {
             RecommendationRunner a = new RecommendationRunner();
             a.getItemsToRate();
             a.printRecommendationsFor("30");
-
         }
 }
