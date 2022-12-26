f9f150e36c8b710cec437ef160a16f883926ef13
Samir <alu0101028491@ull.edu.es>
2022-12-26 10:53:32 +0000

    Documentacion - MovieRunnerAverage

--- src/main/java/recommendation_system/movierunner/MovieRunnerAverage.java
+++ src/main/java/recommendation_system/movierunner/MovieRunnerAverage.java
@@ -2,29 +2,26 @@ package recommendation_system.movierunner;
 
 import recommendation_system.ratings.Rating;
 import recommendation_system.ratings.SecondRatings;
-
 import java.util.ArrayList;
 import java.util.Collections;
-
 import java.lang.System.Logger;
 
-/***************************************************************
- *  Name:    Wei Xu
+/**
+ * <p>
+ *  Class to test the methods created in SecondRatings. - Phase 2
+ *  </p>
  *
- *  Date: Dec 11th, 2019
+ *  <p>
+ *  The class contains the necessary methods for:
+ *  <ul>
+ *  <li> Print average ratings. </li>
+ *  <li> Get average rating of one movie. </li>
+ *  </ul>
+ *  </p>
  *
- *  Description:  -------------------STEP TWO-------------------------
- *                 You will build on this assignment by calculating
- *                 average ratings of movies.
- *                 In this assignment you will modify a new class named SecondRatings,
- *                 which has been started for you, to do many of the calculations
- *                 focusing on computing averages on movie ratings.
- *                 You will also create a second new class named MovieRunnerAverage,
- *                 which you will use to test the methods you created in SecondRatings
- *                 by creating a SecondRatings object in MovieRunnerAverage and
- *                 calling its methods.
- *
- ****************************************************************/
+ *  @since 26/12/22
+ *  @version 1.0
+ */
 public class MovieRunnerAverage {
 
 	private Logger logger = System.getLogger(MovieRunnerAverage.class.getName());
@@ -32,33 +29,33 @@ public class MovieRunnerAverage {
 	private  static Logger loggerStatic = System.getLogger(MovieRunnerAverage.class.getName());
 
 
-    public void printAverageRatings() {
-        SecondRatings sr = new SecondRatings("data/ratedmovies_short.csv", "data/ratings_short.csv");//do i need put filename here?
+    public void printAverageRatings(String ratedMovies, String ratings) {
+        SecondRatings sr = new SecondRatings(ratedMovies, ratings);
         logger.log(Logger.Level.INFO, "Movie size = " + sr.getMovieSize());
         logger.log(Logger.Level.INFO, "Rater size = " + sr.getRaterSize());
         ArrayList<Rating> ratingList = sr.getAverageRatings(2);
         Collections.sort(ratingList);
         for (Rating i : ratingList) {
-        	logger.log(Logger.Level.INFO, "%-10.2f%s%n", i.getValue(), sr.getTitle(i.getItem()));
+        	logger.log(Logger.Level.INFO, i.getValue() + " " + sr.getTitle(i.getItem()));
         }
     }
 
-    public void getAverageRatingOneMovie() {
-        SecondRatings sr = new SecondRatings("data/ratedmovies_short.csv", "data/ratings_short.csv");//do i need put filename here?
+    public void getAverageRatingOneMovie(String movieTitle, String ratedMovies, String ratings) {
+        SecondRatings sr = new SecondRatings(ratedMovies, ratings);
         ArrayList<Rating> ratingList = sr.getAverageRatings(2);
-        String movieTitle = "The Godfather";
         for (Rating i : ratingList) {
             if (sr.getTitle(i.getItem()).equals(movieTitle)) {
-            	logger.log(Logger.Level.INFO, "%-10.2f%s%n", i.getValue(), sr.getTitle(i.getItem()));
+            	logger.log(Logger.Level.INFO, i.getValue() + " " + sr.getTitle(i.getItem()));
             }
         }
     }
 
     public static void main(String[] args) {
         MovieRunnerAverage mra = new MovieRunnerAverage();
-        loggerStatic.log(Logger.Level.INFO, "---------------print test----------------");
-        mra.printAverageRatings();
-        loggerStatic.log(Logger.Level.INFO, "---------------get average rating one movie test----------------");
-        mra.getAverageRatingOneMovie();
+        loggerStatic.log(Logger.Level.INFO, "---printAverageRatings---");
+        mra.printAverageRatings("data/ratedmovies_short.csv", "data/ratings_short.csv");
+        loggerStatic.log(Logger.Level.INFO, "---getAverageRatingOneMovie---");
+        mra.getAverageRatingOneMovie("The Godfather", "data/ratedmovies_short.csv",
+                "data/ratings_short.csv");
     }
 }
